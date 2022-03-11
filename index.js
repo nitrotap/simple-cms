abdaa7997ecf595fc83ef1f782e347c6a42089f4
nitrotap <kartikinpublic@gmail.com>
2022-03-11 02:32:52 -0700

    first route working

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const db = require('./db/connection');
 
 const inquirer = require('inquirer');
-const {express, PORT, app} = require('./server');
+const { express, PORT, app } = require('./server');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+const fetch = require('node-fetch');
+
 
 
 
@@ -24,19 +26,76 @@ async function getMenuOption() { // returns menu choice string
 	return response.menuChoice;
 }
 
-function getDepartments() {
+// function getDepartmentsHTTP() {
+// 	const http = require('http');
 
-	
+// 	const options = {
+// 		'method': 'GET',
+// 		'hostname': 'localhost',
+// 		'port': '3001',
+// 		'path': '/api/dept',
+// 		'headers': {
+// 			'Content-Length': '0'
+// 		}
+// 	};
+
+// 	const req = http.request(options, function (res) {
+// 		const chunks = [];
+
+// 		res.on('data', function (chunk) {
+// 			chunks.push(chunk);
+// 		});
+
+// 		res.on('end', function () {
+// 			const body = Buffer.concat(chunks);
+// 			console.log(body.toString());
+// 			return body.toString();
+// 		});
+// 	});
+
+// 	req.end();
+
+
+// }
+
+async function getDepartments() {
+
+	let url = 'http://localhost:3001/api/dept';
+
+	let options = { method: 'GET' };
+
+	// let a = await fetch(url, options)
+	// 	.then(res => res.json())
+	// 	.then(json => console.log(json))
+	// 	.catch(err => console.error('error:' + err));
+
+	let b = await fetch(url, options)
+		.then(res => res.json());
+	// console.log(b);
+	return b.data;
 }
 
+
 async function main() {
-	let a = await getMenuOption();
-	console.log(a);
-	let b = getDepartments();
-	console.log(b);
+	let menuOption = await getMenuOption();
+	console.log(menuOption);
 
+	switch (menuOption) {
+	case 'view all departments': {
+		let departmentData = await getDepartments();
+		// console.log(departmentData);
 
-	
+		console.table(departmentData);
+		
+		break;
+	}
+
+	default: {
+		console.log('error');
+
+	}
+	}
+
 }
 
 
